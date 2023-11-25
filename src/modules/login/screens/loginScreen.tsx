ccd2e72e37770efb34e959aa75521b5831fbe380
Janio Junior <accorsigasp@hotmail.com>
2023-11-25 00:29:11 -0300

    feat: create first global components

--- src/modules/login/screens/loginScreen.tsx
+++ src/modules/login/screens/loginScreen.tsx
@@ -1,3 +1,4 @@
+import Input from "../../../shared/inputs/input/input";
 import {
   BackgroundImage,
   ContainerLogin,
@@ -12,6 +13,8 @@ const LoginScreen = () => {
       <ContainerLogin>
         <LimitedContainer>
           <LogoImage src="./logo.png" />
+          <Input title="Usuário" />
+          <Input title="Senha" />
         </LimitedContainer>
       </ContainerLogin>
       <BackgroundImage src="./background-login.png" />
