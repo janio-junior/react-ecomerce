b3a3922b272284475062346b31f761461812ccca
Janio Junior <accorsigasp@hotmail.com>
2023-11-29 00:20:09 -0300

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,31 @@
 import "./main.css";
 
+import type { Router as RemixRouter } from "@remix-run/router";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from "react-router-dom";
 
-import App from "./App.tsx";
+import { loginRoutes } from "./modules/login/routes";
+
+export const mainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <>Tela Principal</>,
+    errorElement: <>Página não encontrada!</>,
+  },
+];
+
+const router: RemixRouter = createBrowserRouter([
+  ...mainRoutes,
+  ...loginRoutes,
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
