df7187360efe1da6f65b8c0d89b2a101b8e659c5
Janio Junior <accorsigasp@hotmail.com>
2023-12-10 17:55:40 -0300

    feat: add category in product table

--- src/modules/products/screens/productScreen.tsx
+++ src/modules/products/screens/productScreen.tsx
@@ -1,25 +1,33 @@
 import { ColumnsType } from "antd/es/table";
 import { useEffect } from "react";
 
-import Table from "../../../shared/components/table/table";
+import Table from "../../../shared/components/table/Table";
 import { URL_PRODUCT } from "../../../shared/constants/urls";
 import { MethodsEnum } from "../../../shared/enums/methods.enum";
 import { useDataContext } from "../../../shared/hooks/useDataContext";
 import { useRequests } from "../../../shared/hooks/useRequest";
-import { ProductType } from "../types/ProductType";
+import { ProductType } from "../../../shared/types/ProductType";
+import CategoryColumn from "../components/CategoryColumns";
+import TooltipImage from "../components/TooltipImage";
 
 const columns: ColumnsType<ProductType> = [
   {
     title: "Id",
     dataIndex: "id",
     key: "id",
-    render: (text) => <a>{text}</a>,
+    render: (_, product) => <TooltipImage product={product} />,
   },
   {
     title: "Nome",
     dataIndex: "name",
     key: "name",
     render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Categoria",
+    dataIndex: "category",
+    key: "category",
+    render: (_, product) => <CategoryColumn category={product.category} />,
   },
   {
     title: "Preço",
--- src/shared/hooks/useDataContext.tsx
+++ src/shared/hooks/useDataContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-import { ProductType } from "../../modules/products/types/ProductType";
+import { ProductType } from "../types/ProductType";
 
 export type NotificationType = "success" | "info" | "warning" | "error";
 
